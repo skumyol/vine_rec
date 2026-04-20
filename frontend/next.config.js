dfcacf917c16dcc7aeb6d7e2189067988d9be519
serkan kumyol <skumyol@hotmail.com>
2026-04-20 11:31:26 +0800

    next js redir

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: process.env.NODE_ENV === 'production' ? '/vine' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/vine/' : '',
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8001/api';
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8002/api';
     return [
       {
         source: '/api/:path*',
